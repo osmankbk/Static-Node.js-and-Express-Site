4b1c4d1db9bcbee60434686090132cf760c3451a
osmankbk <32382267+osmankbk@users.noreply.github.com>
2019-11-18 12:51:58 -0500

    restore

--- routes/projects.js
+++ routes/projects.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data/Data.json');
 
-router.get('/:id', (req, res) => {
-  res.render('project', {projs: projects[req.params.id].project_name});
+router.get('/project', (req, res) => {
+  res.render('project', { projs: projects[0].project_name });
 });
 
 module.exports = router;
